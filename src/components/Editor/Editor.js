4418834b47ab666ead9271e057ee15422147e90c
Przemysław Żydek <przemek543@gmail.com>
2018-08-16 20:35:25 +0200

    Add control to editor

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import Container from '../containers/container';
+import TextArea from './textarea/textarea';
+import './editor.scss';
+import Controls from './controls/controls';
 
+/**
+ * Main editor class
+ *
+ * @author Przemysław Żydek
+ * */
 class Editor extends Component {
 
+    state = {
+        /** @var {String} Contains direct value from text editor */
+        value: localStorage.getItem( 'editor_value' ) || ''
+    };
+
+    /**
+     * @var {Object} Contains API methods for child components
+     * */
+    api = {
+        /**
+         * Helper function for getting editor value
+         *
+         * @return String
+         * */
+        getValue: () => this.state.value,
+
+        /**
+         * Helper function for setting text input value state
+         *
+         * @param {String} value
+         *
+         * @return void
+         * */
+        setValue: value => {
+            this.setState( { value } );
+
+            localStorage.setItem( 'editor_value', value );
+        }
+    };
+
     render() {
-        return ();
+
+
+        return (
+            <Container classList="editor-container" height="70%" width="70%">
+                <Controls api={this.api}/>
+                <TextArea api={this.api} value={this.api.getValue()}/>
+            </Container>
+        );
     }
 
 }
